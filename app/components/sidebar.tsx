d700d66cec457bd2ebba99e2d7d37f79fcdf46f7
alex_chang <chang_5241@qq.com>
2023-06-25 09:46:05 +0800

    fit title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          V2AI
         </div>
         <div className={styles["sidebar-sub-title"]}>
           您的免费GPT聊天助手
